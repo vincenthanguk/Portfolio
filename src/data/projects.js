4ec57156f712e1eebc18a67c5c714dc63efe1ad8
vincenthanguk <vincentkreusel@gmail.com>
2021-05-16 16:10:59 +0200

    added project descriptions

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,7 +5,7 @@ const projects = [
     link: "https://lights-out-vincentmax.netlify.app/",
     image: "images/lightsout.png",
     description:
-      "Lights Out game built with react.js. The Board is dynamically created. asd asd asd asd asd lorem lorem lorem",
+      "Lights Out game built with React.js. My biggest challenges in this project were to design a dynamic board with clickable tiles, as well as managing state and event handlers throughout the application. Lights are toggled in a '+' shaped pattern, when the player clicks on a tile. The goal of the game is to switch off all the lights.",
   },
   {
     name: "Solvitur Ambulando",
@@ -13,7 +13,7 @@ const projects = [
     link: "https://solvitur-ambulando.netlify.app/",
     image: "/images/solvitur.png",
     description:
-      "A mindfulness application that allows users to log their personal walking journey on a map. Integrated leaflet library, manipulate the DOM with JS parcel package bundling to hide API key save data into localstorage. Location Services must be activated.",
+      "A mindfulness application that allows users to log their personal walking journey on a map. In this project, I worked with the leaflet map library to display the user's location, as well as allowing interaction with the map. The DOM is manipulated purely with JavaScript, and user data is saved into the browser's local storage.  I used parcel to bundle the application in order to hide the API key via .env file. Location Services must be activated to use the application.",
   },
   {
     name: "McCune-Reischauer Converter",
@@ -21,7 +21,7 @@ const projects = [
     link: "https://mccune-reischauer.netlify.app/",
     image: "images/mccune.png",
     description:
-      "A tool that converts Korean writing into alphabetical letters according to the scientific McCune-Reischauer romanization system. Raw Javascript, making use of Hangul.js module, which disassembles Korean syllable blocks into letters coding many complex cases for transcription",
+      "My first ever Web Application. This tool converts Korean writing into alphabetical letters according to the scientific McCune-Reischauer romanization system. It is written entirely in Javascript and makes use of the Hangul.js module, which disassembles Korean syllable blocks into single letters. The single letters are then checked and converted according to their position within the word. The great number of possible combinations for each letter required careful structuring of if-statements and lengthy testing.",
   },
   {
     name: "Simply Do",
@@ -29,7 +29,7 @@ const projects = [
     link: "https://simply-do.netlify.app/",
     image: "images/simplydo.png",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "A simple Todo list built with React.js. Todo items can be entered, removed and modified. This project taught me the quirks of manipulating state in the React way, i.e. working with pure functions like .map(), .filter() and the rest/destructuring operator '...' .",
   },
 ];
 
