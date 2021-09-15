7ed1af15c87f67038a781707761b97b6a0b708c4
vincenthanguk <vincentkreusel@gmail.com>
2021-09-15 19:41:01 +0200

    added media queries for responsiveness

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
+import "./queries.css";
 import AboutMe from "./components/AboutMe.jsx";
 import Projects from "./components/Projects";
 import NavBar from "./components/NavBar";
